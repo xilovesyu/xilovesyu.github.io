7f36e4498b391833d109f3e4de1afbf6f77c07cd
jxi <jiaxiang.xi@eisgroup.com>
2024-07-10 13:06:15 +0800

    update some styles

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ var scrollToSection = function navigateToSection(id) {
         block: 'center'
     })
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            //拿到指定元素的class，并修改class
+            document.querySelector('.page-nav-space-holder').setAttribute('class', 'page-nav-space-holder');
+        } else {
+            document.querySelector('.page-nav-space-holder').setAttribute('class', 'page-nav-space-holder hidden');
+        }
+    });
+}, { threshold: 0 });
+
+const myElement = document.querySelector('.header');
+observer.observe(myElement);
